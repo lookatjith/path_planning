e69612e6158fae17902387403c669bbae816160f
Ajith Ganesh <a.ganesh@precibake.com>
2018-03-06 21:55:33 +0100

    add astar approach

--- optimal_approaches/a_star.py
+++ optimal_approaches/a_star.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
+import numpy as np
 
+from matplotlib import pyplot as plt
 
 class AStar(object):
     def __init__(self, world_state, obstacles, goal_point, robot_init, pause_sec):
@@ -7,16 +9,76 @@ class AStar(object):
         self._obstacles = obstacles
         self._goal_point = goal_point
         self._robot_init = robot_init
-        self._pause_sec = pause_sec
-
-
-
-
-
-
-        for path in final_path:
-            plot_movement(world_state, obstacles, goal_point, path)
+        self._final_path = []
+        
+        self._final_path = self.optimal_planner()
+        for path in self._final_path:
+            self.plot_movement(self._world_state, self._obstacles, self._goal_point, path)
             plt.pause(pause_sec)            
+
+    def optimal_planner(self):
+        """
+        Method uses A star approach to find the optimal way to reach the
+        goal point from the starting point
+
+        Advantages
+        ----------
+        1) compared to the random approach, optimal planner finds the path
+        in a short time
+        2) Does not get stuck in between obstacles
+        """
+
+        open_path = {}
+        closed_path = {}
+
+        f = 0
+        d = 0
+
+        open_path[self._robot_init] = f
+        successors = []
+
+        while open_path is not bool(open_path):
+            # get the minimum value of minimum key
+            min_val = min(open_path.itervalues())
+            min_keys = [key for key, value in open_path.iteritems()
+                        if value == min_val]
+            pos = min_keys[0]
+            # for key in min_keys:
+            open_path = {}
+            #dist_start = open_path[pos]
+            dist_start = min_val
+            # generate the successors
+            successors = [(pos[0], pos[1] + 1), (pos[0] + 1, pos[1]),
+                          (pos[0], pos[1] - 1), (pos[0] - 1, pos[1])]
+            for successor in successors:
+                #### checking starts #####
+                # check for world state
+                if successor in self._world_state:
+                    if successor not in self._obstacles:
+                        if successor != self._goal_point:
+                            # f(n) = g(n) + h(n)
+                            # calculate h(n)
+                            dist_to_goal = np.sqrt(np.square(
+                                self._goal_point[0] - successor[0]) + np.square(self._goal_point[1] - successor[1]))
+                            # calculate g(n)
+                            dist = dist_start + \
+                                np.sqrt(
+                                    np.square(successor[0] - pos[0]) + np.square(successor[1] - pos[1]))
+                            cost = dist_to_goal + dist
+                            # check open list cost in successor
+                            if successor not in closed_path.values():
+                                if successor not in open_path or cost < open_path[successor]:
+                                    open_path[successor] = cost
+                                elif successor in closed_path and closed_path[successor] > cost:
+                                    open_path[successor] = cost
+                        else:
+                            print("goal point reached")
+                            closed_path[cost] = pos
+                            self._final_path.append(pos)
+                            return self._final_path
+
+            closed_path[cost] = pos
+            self._final_path.append(pos)
 
     def plot_movement(self, world_state, obstacles, goal_point, robot_init):
         """ Plot environment including robot and obstacles """
--- rob_simulator.py
+++ rob_simulator.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 from random_approach.random_planner import RandomPlanner
+from optimal_approaches.a_star import AStar
 from matplotlib import pyplot as plt
 from matplotlib import *
-import numpy as np
 
 
 class RobSimulator(object):
@@ -26,8 +26,6 @@ class RobSimulator(object):
         plt.ion()
         # plot to pause in between
         pause_sec = 1
-        # final path useful for optimal approach
-        final_path = []
 
         if approach == "random":
             random_planner = RandomPlanner(
@@ -35,79 +33,8 @@ class RobSimulator(object):
             random_planner.random_planner(
                 x=robot_init[0], y=robot_init[1], new_x=robot_init[0], new_y=robot_init[1])
         elif approach == "a_star":
-            a_star = AStar(max_step_number, world_state, obstacles, goal_point, robot_init, pause_sec)
-            a_star.a_star(final_path)
-            final_path = optimal_planner()
-
-    def optimal_planner():
-        """
-        Method uses A star approach to find the optimal way to reach the
-        goal point from the starting point
-
-        Advantages
-        ----------
-        1) compared to the random approach, optimal planner finds the path
-        in a short time
-        2) Does not get stuck in between obstacles
-        """
-
-        open_path = {}
-        closed_path = {}
-
-        f = 0
-        d = 0
-
-        open_path[robot_init] = f
-        successors = []
-
-        while open_path is not bool(open_path):
-            # get the minimum value of minimum key
-            min_val = min(open_path.itervalues())
-            min_keys = [key for key, value in open_path.iteritems()
-                        if value == min_val]
-            pos = min_keys[0]
-            # for key in min_keys:
-            open_path = {}
-            #dist_start = open_path[pos]
-            dist_start = min_val
-            # generate the successors
-            successors = [(pos[0], pos[1] + 1), (pos[0] + 1, pos[1]),
-                          (pos[0], pos[1] - 1), (pos[0] - 1, pos[1])]
-            for successor in successors:
-                #### checking starts #####
-                # check for world state
-                if successor in world_state:
-                    if successor not in obstacles:
-                        if successor != goal_point:
-                            # f(n) = g(n) + h(n)
-                            # calculate h(n)
-                            dist_to_goal = np.sqrt(np.square(
-                                goal_point[0] - successor[0]) + np.square(goal_point[1] - successor[1]))
-                            # calculate g(n)
-                            dist = dist_start + \
-                                np.sqrt(
-                                    np.square(successor[0] - pos[0]) + np.square(successor[1] - pos[1]))
-                            cost = dist_to_goal + dist
-                            # check open list cost in successor
-                            if successor not in closed_path.values():
-                                if successor not in open_path or cost < open_path[successor]:
-                                    open_path[successor] = cost
-                                elif successor in closed_path and closed_path[successor] > cost:
-                                    open_path[successor] = cost
-                        else:
-                            print("goal point reached")
-                            closed_path[cost] = pos
-                            final_path.append(pos)
-                            return final_path
-
-            closed_path[cost] = pos
-            final_path.append(pos)
-
-
-
-    # run_random_approach()
-    # run_optimal_approach()
-
+            a_star = AStar(world_state, obstacles, goal_point, robot_init, pause_sec)
+            
 
 if __name__ == '__main__':
     # get the approach name from the user

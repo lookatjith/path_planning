86ce3ef4e0a7b15e1b054bfac9ab8726f3fe3035
Ajith Ganesh <a.ganesh@precibake.com>
2018-03-08 23:10:58 +0100

    make python module

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='path_planning',
+      version='1.0',
+      description='path planning algorithms for a robot',
+      author='Ajith Madhav',
+      author_email='s@s.com',
+      packages=['random_approach', 'optimal_approaches'],
+     )
---
+++ tests/random_approach_unit_test.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python
+from random_approach.random_planner import RandomPlanner

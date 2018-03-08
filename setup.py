299776fc5aa8127a8ae4add5ee8627914bbc9ab5
Ajith <lookatajith@gmail.com>
2018-03-08 23:13:05 +0100

    add email

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name='path_planning',
       version='1.0',
       description='path planning algorithms for a robot',
       author='Ajith Madhav',
-      author_email='s@s.com',
+      author_email='name@example.com',
       packages=['random_approach', 'optimal_approaches'],
      )
